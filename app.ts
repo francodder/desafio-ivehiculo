4569522dd31d6640bd53765813a139f66b20da3b
franRicoso <frocchia@encodesa.com.ar>
2021-10-31 13:34:37 -0300

    Publicación del proyecto

---
+++ app.ts
@@ -0,0 +1,58 @@
+interface Vehiculo {
+    avanzar(): void,
+    frenar(): void,
+}
+
+interface VehiculoMotorizado extends Vehiculo {
+    arrancarVehiculo(): void,
+    encenderLuces(): void,
+    apagarLuces(): void,
+    hacerSonarBocina(): void,
+}
+
+interface IVehiculo extends VehiculoMotorizado {
+    endenderRadio(): void,
+    apagarRadio(): void
+}
+
+// --------------------------------------------------------------
+
+class Camion implements IVehiculo {
+
+    arrancarVehiculo(): void {}
+    avanzar(): void {}
+    frenar(): void {}
+    encenderLuces(): void {}
+    apagarLuces(): void {}
+    endenderRadio(): void {}
+    apagarRadio(): void {}
+    hacerSonarBocina(): void {}
+}
+
+class Auto implements IVehiculo {
+
+    arrancarVehiculo(): void {}
+    avanzar(): void {}
+    frenar(): void {}
+    encenderLuces(): void {}
+    apagarLuces(): void {}
+    endenderRadio(): void {}
+    apagarRadio(): void {}
+    hacerSonarBocina(): void {}
+}
+
+class Monopatin implements VehiculoMotorizado {
+
+    arrancarVehiculo(): void {}
+    avanzar(): void {}
+    frenar(): void {}
+    encenderLuces(): void {}
+    apagarLuces(): void {}
+    hacerSonarBocina(): void {}
+}
+
+class Bicicleta implements Vehiculo {
+
+    avanzar(): void {}
+    frenar(): void {}
+}
---
+++ dist/app.js
@@ -0,0 +1,34 @@
+"use strict";
+// --------------------------------------------------------------
+class Camion {
+    arrancarVehiculo() { }
+    avanzar() { }
+    frenar() { }
+    encenderLuces() { }
+    apagarLuces() { }
+    endenderRadio() { }
+    apagarRadio() { }
+    hacerSonarBocina() { }
+}
+class Auto {
+    arrancarVehiculo() { }
+    avanzar() { }
+    frenar() { }
+    encenderLuces() { }
+    apagarLuces() { }
+    endenderRadio() { }
+    apagarRadio() { }
+    hacerSonarBocina() { }
+}
+class Monopatin {
+    arrancarVehiculo() { }
+    avanzar() { }
+    frenar() { }
+    encenderLuces() { }
+    apagarLuces() { }
+    hacerSonarBocina() { }
+}
+class Bicicleta {
+    avanzar() { }
+    frenar() { }
+}
